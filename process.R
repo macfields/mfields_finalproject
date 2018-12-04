b3342bc367c7c3cb193273cc7962aac42f9261e0
Maclaine Fields <maclainefields@Maclaines-MacBook-Pro.local>
2018-12-04 13:50:17 -0500

    including zip

--- process.R
+++ process.R
@@ -5,8 +5,8 @@ library(scales)
 library(fs)
 library(lubridate)
 
-
-
+#unzipping file and putting datavolley files in folder. 
+unzip("redatavolleyfiles.zip", exdir = "2018dvwfiles/")
 
 #I know there must be an easier name to read in these datavolley files. 
 #However, given that there are only 21, I can do it by hand individually. Since the Harvard Women's 
